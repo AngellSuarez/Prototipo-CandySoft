138ffb74d9d845c41e194c67169e9715fcfc488a
angelsuaz <139772791+AngellSuarez@users.noreply.github.com>
2025-09-08 12:30:20 -0500

    Update vite.config.js 1

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,  // Listen on all network interfaces
-    preview: {
-      allowedHosts: ['prototipo-candysoft.onrender.com','prototipo-candysoft.onrender.com'], // Add your Render hostname here
-    },
+  preview: {
+    host: true,
+    port: 8080, 
+    allowedHosts: ['prototipo-candysoft.onrender.com'],
   },
-})
+  server: {
+    host: true,
+  },
+});
